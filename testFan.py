9edd96f6e8b2d434799375281989566e4ee16a7a
Aleksander <skret002@mail.ru>
2022-02-10 00:07:02 +0300

    Update testFan.py

--- testFan.py
+++ testFan.py
@@ -9,19 +9,10 @@ def testFan(id_rig):
     for chip in sensors.iter_detected_chips():      
         numGpu = numGpu+1                   
         if 'amd' in str(chip):
-            print(chip)                                                              
-        #if str(chip.adapter_name) == "PCI adapter":                                                                                   
-        #    for feature in chip:
-        #        try:                                                                                                                                                                                           
-        #            if str(feature.label) == "edge":                                                                                             
-        #                #print(feature.get_value())     # температура
-        #                temp_gpu.append(round(feature.get_value()))
-        #        except Exception:
-        #            print("Ошибка получения Температуры AMD")
-        #            numGpu -1
-        #        try:
-        #            if str(feature.label) == "fan1": 
-        #                #print(feature.get_value())    # скорость кулеров
+            print(chip)                                                                                                                                                
+            for feature in chip:
+                if str(feature.label) == "fan1": 
+                    print(feature.get_value())    # скорость кулеров
         #                #rpm_fun_gpu.append({str(numGpu):feature.get_value()})
 
         #                rpm_fun_gpu[str(numGpu)] = feature.get_value()

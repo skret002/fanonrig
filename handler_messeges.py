8d9bdc99668188e95fa8aab7b9bbd95e94f8e3ec
Aleksander <skret002@mail.ru>
2022-02-21 03:15:08 +0300

    Update handler_messeges.py

--- handler_messeges.py
+++ handler_messeges.py
@@ -1,13 +1,11 @@
 import requests  
 
 def transmit_mess(messege, rig_id): 
-	print("Сообщение на сервер", messege)
-	requests.post('http://ggc.center:8000/meseger/', data = {'id_rig': rig_id, 
+    print("Сообщение на сервер", messege)
+    requests.post('http://ggc.center:8000/meseger/', data = {'id_rig': rig_id, 
                                                                 'messege':messege,
                                                                  })
-	os.system("reboot")
-    subprocess.getstatusoutput("sreboot")
-	return()
+    return()
 
 if __name__ == '__main__':
     transmit_mess()

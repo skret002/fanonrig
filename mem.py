e0cf2caed0c429ed9b8aa9226fe718e61e3398c7
Aleksandr <skret002@mail.ru>
2022-04-21 20:56:01 +0300

    add

---
+++ mem.py
@@ -0,0 +1,28 @@
+import sensors, json, subprocess                                                                                                                                       
+sensors.init()                                                                                                                                                                                                                                                                          
+
+def mem_temp():                                                                                                                                      
+    mem_temp = []
+    #ищем температуру памяти   - только hiveOS           
+    try:  
+        (status,output)=subprocess.getstatusoutput("cat /run/hive/fan_controller_req")     
+        a =  json.loads(output)
+        for memt in a["gpu_mtemp"]:
+            mem_temp.append(int(memt))
+    except Exception as e:   # если ошибка найдет только AMD
+        print('error memt ', e)
+        mem_temp = []
+        for chip in sensors.iter_detected_chips():                                                                                                       
+            for feature in chip:                                                                                                                         
+                if str(feature.label) == "mem":   
+                    try:                                                                                                       
+                        mem_temp.append(feature.get_value()) 
+                    except Exception:
+                        mem_temp.append(0)  
+
+     #ищем температуру на зеленых  картах   
+
+    return(max(mem_temp))                                                                                                                            
+
+if __name__ == '__main__':                                                                                                                           
+    mem_temp()                                                                                                                                       

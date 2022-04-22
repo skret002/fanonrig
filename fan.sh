8d598861a9fce256fdd9b00db8b1234569530690
Aleksander <skret002@mail.ru>
2022-04-22 14:31:07 +0300

    Update fan.sh

--- fan.sh
+++ fan.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sleep 2
 echo 1 >>/sys/class/hwmon/hwmon1/pwm2_enable
 echo 15 >> /sys/class/hwmon/hwmon1/pwm2
 sleep 5
 cd /home/onrig/
-sleep 60
+sleep 40
 sudo chmod ugo+x engine.bin
 #sudo nice -n -20 ./engine.bin
 ./engine.bin

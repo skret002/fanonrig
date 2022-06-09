0f4c1498a5aeae00fb49ea813ee22d91ad7feab2
Aleksander <skret002@mail.ru>
2022-06-09 22:32:09 +0300

    Update fan.sh

--- fan.sh
+++ fan.sh
@@ -2,7 +2,6 @@
 sleep 5
 echo 1 >>/sys/class/hwmon/hwmon1/pwm2_enable
 echo 15 >> /sys/class/hwmon/hwmon1/pwm2
-sleep 5
 cd /home/onrig/
 sudo chmod ugo+x pre_init_gpu.bin
 ./pre_init_gpu.bin
@@ -12,4 +11,5 @@ sleep 45
 sudo chmod ugo+x engine.bin
 ./engine.bin
 
+sudo systemctl start  keeper_fan
 #keeper > keeper_fan

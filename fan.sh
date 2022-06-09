c72f27bddb82080c08e1a7b166a84afcf481f2b3
Aleksander <skret002@mail.ru>
2022-06-09 22:21:44 +0300

    add

---
+++ fan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sleep 5
+echo 1 >>/sys/class/hwmon/hwmon1/pwm2_enable
+echo 15 >> /sys/class/hwmon/hwmon1/pwm2
+sleep 5
+cd /home/onrig/
+sudo chmod ugo+x pre_init_gpu.bin
+./pre_init_gpu.bin
+sudo chmod ugo+x test_pci.bin
+./test_pci.bin
+sleep 45
+sudo chmod ugo+x engine.bin
+./engine.bin
+
+#keeper > keeper_fan
